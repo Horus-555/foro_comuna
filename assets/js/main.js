a8a07d088c29f0878ed23f8093a53776bae645e2
Horus-555 <c.horus.t.0107@gmail.com>
2025-12-06 07:12:27 -0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// Scripts base, puedes agregar más funciones aquí.
+
+console.log("JS cargado correctamente.");
+
+// Ejemplo: Confirmar acciones peligrosas
+function confirmar(mensaje) {
+    return confirm(mensaje);
+}
